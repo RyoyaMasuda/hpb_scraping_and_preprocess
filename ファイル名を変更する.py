1f24dffca14e3e4e97f63fd6b9ae13c7faf7ae42
Ryoya Masuda <despocampi.foot@gmail.com>
2023-07-01 14:39:54 +0900

    remove file

--- ファイル名を変更する.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-DATA_PATH = Path('./data/完成')
-for file_path in DATA_PATH.glob('*.csv'):
-    print(file_path.name)
-    
-    updated_name = file_path.name.replace('の美容院・美容室・ヘアサロン', '')
-    print(updated_name)
-    file_path.rename(updated_name)
